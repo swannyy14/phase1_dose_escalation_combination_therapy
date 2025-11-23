1d9d5ae2d67769bf9de1a9ad81bf7a9edc8dd1b3
Alena Z <31553401+snanalice@users.noreply.github.com>
2025-11-23 15:55:58 -0500

    Add files via upload
    Functions for the generating dose grid, the partial ordering matrix (po_mat), and the skeleton matrix (skeleton_mat), given an input matrix of true toxicity probabilities (true_tox_mat).

---
+++ R/generate_mat.R
@@ -0,0 +1,76 @@
+generate_dose_grid <- function(true_tox_mat) {
+  nr <- nrow(true_tox_mat)
+  nc <- ncol(true_tox_mat)
+  
+  grid <- matrix(1:(nr * nc), nrow = nr, ncol = nc, byrow = TRUE)
+}
+
+generate_orderings_matrix <- function(grid) {
+  po_mat <- matrix(NA, nrow = 6, ncol = ncol(grid)*nrow(grid))
+  nr <- nrow(grid)
+  nc <- ncol(grid)
+  
+  # 1. Across Rows 
+  po_mat[1, ] <- as.vector(t(grid))
+  
+  # 2. Up Columns 
+  po_mat[2, ] <- as.vector(grid)
+  
+  # 3. Up Diagonals 
+  po_mat[3, ] <- unlist(lapply(2:(nr+nc), function(s) {
+    unlist(mapply(function(i, j) {
+      if (i >= 1 && i <= nr && j >= 1 && j <= nc && (i + j) == s) grid[i, j]
+    }, rep(1:nr, each=nc), rep(1:nc, times=nr)))
+  }))
+  
+  # 4. Down Diagonals 
+  po_mat[4, ] <- unlist(lapply((1-nc):(nr-1), function(d) {
+    unlist(mapply(function(i, j) {
+      if (i >= 1 && i <= nr && j >= 1 && j <= nc && (i - j) == d) grid[i, j]
+    }, rep(1:nr, each=nc), rep(1:nc, times=nr)))
+  }))
+  
+  # 5. Down-Up Diagonals
+  po_mat[5, ] <- {
+    result <- c()
+    for (k in 1:(nr + nc - 1)) {
+      temp <- c()
+      for (i in 1:nr) {
+        j <- k - i + 1
+        if (j >= 1 && j <= nc) {
+          temp <- c(temp, grid[i, j])
+        }
+      }
+      result <- c(result, temp)
+    }
+    result
+  }
+  
+  # 6. Up-Down Diagonals
+  po_mat[6, ] <- {
+    result <- c()
+    for (k in 1:(nr + nc - 1)) {
+      temp <- c()
+      for (i in nr:1) {
+        j <- k - i + 1
+        if (j >= 1 && j <= nc) {
+          temp <- c(temp, grid[i, j])
+        }
+      }
+      result <- c(result, temp)
+    }
+    result
+  }
+  
+  # Remove duplicate ordering
+  po_mat <- unique(po_mat)
+  
+  return(po_mat)
+}
+
+generate_skeleton_mat <- function(po_mat) {
+  skeleton_mat <- matrix(
+    rep(c(0.05, 0.08, 0.10, 0.13, 0.15, 0.18, 0.22, 0.25, 0.35), nrow(po_mat)),
+    nrow = nrow(po_mat), ncol = ncol(po_mat), byrow = TRUE
+  )
+}
