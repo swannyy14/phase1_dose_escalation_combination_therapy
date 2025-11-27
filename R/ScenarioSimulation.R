5e61892ed1db73409a99adb1e9d382702c34af7d
NandhanaNambi <nandhana@umich.edu>
2025-11-27 13:21:16 -0500

    updated 9 scenarios

--- R/ScenarioSimulation.R
+++ R/ScenarioSimulation.R
@@ -1,332 +1,112 @@
-generate_dose_grid <- function(true_tox_mat) {
-  nr <- nrow(true_tox_mat)
-  nc <- ncol(true_tox_mat)
-  grid <- matrix(1:(nr * nc), nrow = nr, ncol = nc, byrow = TRUE)
-}
-
-generate_orderings_matrix <- function(grid) {
-  po_mat <- matrix(NA, nrow = 6, ncol = ncol(grid)*nrow(grid))
-  nr <- nrow(grid)
-  nc <- ncol(grid)
-  po_mat[1, ] <- as.vector(t(grid))
-  po_mat[2, ] <- as.vector(grid)
-  po_mat[3, ] <- unlist(lapply(2:(nr+nc), function(s) {
-    unlist(mapply(function(i, j) {
-      if (i >= 1 && i <= nr && j >= 1 && j <= nc && (i + j) == s) grid[i, j]
-    }, rep(1:nr, each=nc), rep(1:nc, times=nr)))
-  }))
-  po_mat[4, ] <- unlist(lapply((1-nc):(nr-1), function(d) {
-    unlist(mapply(function(i, j) {
-      if (i >= 1 && i <= nr && j >= 1 && j <= nc && (i - j) == d) grid[i, j]
-    }, rep(1:nr, each=nc), rep(1:nc, times=nr)))
-  }))
-  po_mat[5, ] <- {
-    result <- c()
-    for (k in 1:(nr + nc - 1)) {
-      temp <- c()
-      for (i in 1:nr) {
-        j <- k - i + 1
-        if (j >= 1 && j <= nc) {
-          temp <- c(temp, grid[i, j])
-        }
-      }
-      result <- c(result, temp)
-    }
-    result
-  }
-  po_mat[6, ] <- {
-    result <- c()
-    for (k in 1:(nr + nc - 1)) {
-      temp <- c()
-      for (i in nr:1) {
-        j <- k - i + 1
-        if (j >= 1 && j <= nc) {
-          temp <- c(temp, grid[i, j])
-        }
-      }
-      result <- c(result, temp)
-    }
-    result
-  }
-  po_mat <- unique(po_mat)
-  return(po_mat)
-}
-
-generate_skeleton_mat <- function(po_mat, skeleton_vec) {
-  d <- length(skeleton_vec)
-  s <- nrow(po_mat)
-  skeleton_mat <- matrix(0, nrow = s, ncol = d)
-  for (j in 1:s) {
-    skeleton_mat[j, ] <- skeleton_vec[order(po_mat[j, ])]
-  }
-  return(skeleton_mat)
-}
-
+library(tidyverse)
 library(nnet)
+library(knitr)
+library(kableExtra)
+library(dfcrm)
 
-calc_log_lik <- function(a, response, alph, weights) {
-  sum(response*a*log(alph) +
-        (1-response)*log(1 - weights*(alph)^a))
-}
+# choose the two helper files (first: po_tite_crm.R, second: generate_mat.R)
+source(file.choose())
+source(file.choose())
 
-optimize_log_lik <- function(response, alph, weights) {
-  a_mle <- optimize(
-    f = calc_log_lik, interval = c(0, 100),
-    response = response,
-    alph = alph,
-    weights = weights,
-    maximum = TRUE
-  )
-  return(a_mle)
-}
+po_mat <- matrix(NA, nrow = 6, ncol = 9)
+po_mat[1, ] <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+po_mat[2, ] <- c(1, 4, 7, 2, 5, 8, 3, 6, 9)
+po_mat[3, ] <- c(1, 2, 4, 3, 5, 7, 6, 8, 9)
+po_mat[4, ] <- c(1, 4, 2, 7, 5, 3, 8, 6, 9)
+po_mat[5, ] <- c(1, 2, 4, 7, 5, 3, 6, 8, 9)
+po_mat[6, ] <- c(1, 4, 2, 3, 5, 7, 8, 6, 9)
 
-calc_weight <- function(current_time, followup_time) {
-  return(min(current_time, followup_time))
-}
+skeleton_mat <- matrix(
+  rep(c(0.05, 0.08, 0.10, 0.13, 0.15, 0.18, 0.22, 0.25, 0.35),
+      times = nrow(po_mat)),
+  nrow = nrow(po_mat), ncol = ncol(po_mat), byrow = TRUE
+)
 
-enroll_patient <- function(
-    patient_data,
-    enrollment_date,
-    assigned_dose,
-    true_tox_vec,
-    n = NULL,
-    verbose = FALSE
-) {
-  n_enrolled <- patient_data$n_enrolled
-  if (is.null(n)) {
-    if (n_enrolled == (nrow(patient_data$data) - 1)) {
-      n <- 1
-    } else {
-      n <- sample(c(1,2), 1)
-    }
-  }
-  for (i in 1:n) {
-    if (verbose)
-      print(paste0(
-        "Enrolling new patient: date - ", enrollment_date,
-        ", dose - ", assigned_dose,
-        ", toxic prob - ", true_tox_vec[assigned_dose])
-      )
-    patient_data$data[n_enrolled + i, "enrollment_date"] <- enrollment_date
-    patient_data$data[n_enrolled + i, "dose_assignment"] <- assigned_dose
-    tox <- rbinom(1, 1, true_tox_vec[assigned_dose])
-    patient_data$data[n_enrolled + i,"tox"] <- tox
-    if (tox == 1) {
-      patient_data$data[n_enrolled + i,"time_to_tox"] <- sample(56, 1)
-    } else {
-      patient_data$data[n_enrolled + i,"time_to_tox"] <- 0
-    }
-  }
-  patient_data$n_enrolled <- n + patient_data$n_enrolled
-  return(patient_data)
-}
-
-get_next_dose_po_tite_crm <- function(
-    patient_data,
-    current_date,
-    po_mat,
-    skeleton_mat,
-    target_pt = 0.25
-) {
-  n_enrolled <- patient_data$n_enrolled
-  M <- nrow(po_mat)
-  response <- patient_data$data$tox[1:n_enrolled]
-  tox_time <- patient_data$data$time_to_tox[1:n_enrolled]
-  enrollment_dates <- patient_data$data$enrollment_date[1:n_enrolled]
-  i <- (tox_time > 30 | response == 0) & ((current_date - enrollment_dates) < 56)
-  if (sum(i) >= 1) {
-    response[i] <- 0
-    weights <- rep(1, length(response))
-    weights[i] <- 30 / 56
-  } else {
-    weights <- rep(1, length(response))
-  }
-  dose_assignment <- patient_data$data$dose_assignment[1:n_enrolled]
-  a_mle <- numeric(M)
-  log_lik <- numeric(M)
-  for (m in 1:M) {
-    partial_order <- po_mat[m,]
-    alphas <- skeleton_mat[m,match(dose_assignment, partial_order)]
-    optimized <- optimize_log_lik(response = response, alph = alphas, weights = weights)
-    a_mle[m] <- optimized$maximum
-    log_lik[m] <- optimized$objective
-  }
-  chosen_po <- which.is.max(log_lik)
-  po <- po_mat[chosen_po,]
-  est_p <- skeleton_mat[chosen_po,]^a_mle[chosen_po]
-  if (est_p[1] <= target_pt) {
-    next_dose <- po[max(which(est_p <= target_pt))]
-  } else {
-    next_dose <- 1
-  }
-  return(list(
-    next_dose = next_dose,
-    partial_order = chosen_po,
-    a_mle = a_mle[chosen_po]
-  ))
-}
-
-begin_sim <- function(
-    true_tox_vec,
-    po_mat,
-    skeleton_mat,
-    target_pt = 0.25,
-    max_patients = 25,
-    verbose = FALSE
-) {
-  patient_data <- list(
-    data = data.frame(
-      patient_id = seq(1, max_patients),
-      enrollment_date = NA,
-      dose_assignment = NA,
-      tox = NA,
-      time_to_tox = NA
-    ),
-    n_enrolled = 0
-  )
-  is_step1_phase <- TRUE
-  current_date <- 0
-  next_dose <- 1
-  zone <- 1
-  treatment_esc_order <- c(
-    1,2,4,3,5,7,6,8,9
-  )
-  if (verbose) print("Begin step 1...")
-  while (is_step1_phase & patient_data$n_enrolled < max_patients) {
-    patient_data <- enroll_patient(
-      patient_data,
-      enrollment_date = current_date,
-      assigned_dose = next_dose,
-      true_tox_vec = true_tox_vec,
-      n = 2,
-      verbose = verbose
-    )
-    current_date <- current_date + 30
-    response <- patient_data$data$tox[1:patient_data$n_enrolled]
-    tox_time <- patient_data$data$time_to_tox[1:patient_data$n_enrolled]
-    enrollment_dates <- patient_data$data$enrollment_date[1:patient_data$n_enrolled]
-    censored <- enrollment_dates + tox_time > current_date
-    if (any(censored)) {
-      response[censored] <- 0
-    }
-    if (length(unique(response)) > 1) {
-      if (verbose) {
-        print("Both toxicity and non-toxicity observed")
-      }
-      is_step1_phase <- FALSE
-    } else if (all(response == 1)) {
-      if (verbose) print("Both toxicity observed at beginning")
-      sim_result <- list(
-        data = patient_data$data,
-        n_enrolled = patient_data$n_enrolled,
-        MTD = 1
-      )
-      return(sim_result)
-    } else {
-      if (zone < 9) {
-        zone <- zone + 1
-        next_dose <- treatment_esc_order[[zone]][sample(length(treatment_esc_order[[zone]]), 1)]
-      } else {
-        if (!all(tox_time == 0)) {
-          current_date <- 30
-          is_step1_phase <- FALSE
-        } else {
-          sim_result <- list(
-            data = patient_data$data,
-            n_enrolled = patient_data$n_enrolled,
-            MTD = 9
-          )
-          return(sim_result)
-        }
-      }
-    }
-  }
-  if (verbose) print("Moving on to step 2...")
-  po_tite_crm_out <- get_next_dose_po_tite_crm(
-    patient_data = patient_data,
-    current_date = current_date,
-    po_mat = po_mat,
-    skeleton_mat = skeleton_mat,
-    target_pt = target_pt
-  )
-  next_dose <- po_tite_crm_out$next_dose
-  while (patient_data$n_enrolled < max_patients) {
-    patient_data <- enroll_patient(
-      patient_data,
-      enrollment_date = current_date,
-      assigned_dose = next_dose,
-      true_tox_vec = true_tox_vec,
-      verbose = verbose
-    )
-    current_date <- current_date + 30
-    if (patient_data$n_enrolled > max_patients) {
-      stop("n_enrolled cannot be higher than max_patients")
-    }
-    if (patient_data$n_enrolled != max_patients) {
-      po_tite_crm_out <- get_next_dose_po_tite_crm(
-        patient_data = patient_data,
-        current_date = current_date,
-        po_mat = po_mat,
-        skeleton_mat = skeleton_mat
-      )
-    } else {
-      po_tite_crm_out <- get_next_dose_po_tite_crm(
-        patient_data = patient_data,
-        current_date = current_date + 30,
-        po_mat = po_mat,
-        skeleton_mat = skeleton_mat
-      )
-    }
-    next_dose <- po_tite_crm_out$next_dose
-  }
-  sim_result <- list(
-    data = patient_data$data,
-    n_enrolled = patient_data$n_enrolled,
-    MTD = next_dose
-  )
-  return(sim_result)
-}
+target_pt <- 0.25
+max_patients <- 25
 
 simulate_trials <- function(
     true_tox_vec,
     po_mat,
     skeleton_mat,
     target_pt = 0.25,
-    N = 100,
-    verbose = FALSE
+    max_patients = 25,
+    N = 1000,
+    verbose = FALSE,
+    include_patient_data = FALSE
 ) {
   operating_char <- list(
     MTD = integer(N),
     n_enrolled = integer(N),
-    adverse_event_rate = numeric(N)
+    adverse_event_rate = numeric(N),
+    max_day = integer(N),
+    n_assigned_by_dose = integer(length(true_tox_vec)),
+    n_dlt_by_dose = integer(length(true_tox_vec)),
+    n_sim = N
   )
+
+  if (include_patient_data) {
+    operating_char$patient_data <- list()
+  }
+
   for (i in 1:N) {
     sim_data <- begin_sim(
       true_tox_vec = true_tox_vec,
       po_mat = po_mat,
       skeleton_mat = skeleton_mat,
-      target_pt = 0.25,
+      target_pt = target_pt,
+      max_patients = max_patients,
       verbose = verbose
     )
-    operating_char[["MTD"]][i] <- sim_data$MTD
-    operating_char[["n_enrolled"]][i] <- sim_data$n_enrolled
-    operating_char[["adverse_event_rate"]][i] <- mean(sim_data$data$tox, na.rm = TRUE)
 
+    if (include_patient_data) {
+      operating_char$patient_data[[i]] <- sim_data$data
+    }
+
+    operating_char$MTD[i] <- sim_data$MTD
+    operating_char$n_enrolled[i] <- sim_data$n_enrolled
+    operating_char$adverse_event_rate[i] <- mean(sim_data$data$tox, na.rm = TRUE)
+    operating_char$max_day[i] <- max(sim_data$data$enrollment_date, na.rm = TRUE)
+
+    dose_data <- sim_data$data %>%
+      filter(!is.na(dose_assignment)) %>%
+      group_by(dose_assignment) %>%
+      summarise(
+        n = n(),
+        n_dlt = sum(tox),
+        .groups = "drop"
+      )
+
+    operating_char$n_assigned_by_dose[dose_data$dose_assignment] <-
+      operating_char$n_assigned_by_dose[dose_data$dose_assignment] + dose_data$n
+
+    operating_char$n_dlt_by_dose[dose_data$dose_assignment] <-
+      operating_char$n_dlt_by_dose[dose_data$dose_assignment] + dose_data$n_dlt
   }
-  return(operating_char)
+
+  operating_char
 }
 
-dummy_mat <- matrix(0, nrow = 3, ncol = 3)
-grid <- generate_dose_grid(dummy_mat)
-po_mat <- generate_orderings_matrix(grid)
+clean_results <- function(op_char, true_tox_vec) {
+  cat("Average number of patients enrolled:", mean(op_char$n_enrolled), "\n")
+  cat("Average last enrollment day:", mean(op_char$max_day), "\n")
 
-skeleton_vec <- c(
-  0.05, 0.08, 0.10,
-  0.13, 0.15, 0.18,
-  0.22, 0.25, 0.35
+  tibble(
+    Dose = names(true_tox_vec),
+    true_p_dlt = true_tox_vec,
+    p_select_mtd = as.numeric(
+      table(factor(op_char$MTD, levels = seq_along(true_tox_vec)))
+    ) / op_char$n_sim,
+    avg_num_assigned = op_char$n_assigned_by_dose / op_char$n_sim,
+    avg_num_dlt = op_char$n_dlt_by_dose / op_char$n_sim
+  )
+}
+
+dose_names <- c(
+  "P0.5+H2", "P0.5+H3", "P0.5+H5",
+  "P0.75+H2", "P0.75+H3", "P0.75+H5",
+  "P1.0+H2", "P1.0+H3", "P1.0+H5"
 )
-
-skeleton_mat <- generate_skeleton_mat(po_mat, skeleton_vec)
 
 scen1 <- c(
   0.05, 0.08, 0.10,
@@ -347,22 +127,78 @@ scen3 <- c(
 )
 
 scen4 <- c(
-  0.05, 0.12, 0.22,
-  0.06, 0.14, 0.28,
-  0.08, 0.16, 0.35
+  0.05, 0.06, 0.08,
+  0.12, 0.14, 0.16,
+  0.22, 0.28, 0.35
 )
 
-set.seed(123)
-oc1 <- simulate_trials(scen1, po_mat, skeleton_mat, target_pt = 0.25, N = 500)
-oc2 <- simulate_trials(scen2, po_mat, skeleton_mat, target_pt = 0.25, N = 500)
-oc3 <- simulate_trials(scen3, po_mat, skeleton_mat, target_pt = 0.25, N = 500)
-oc4 <- simulate_trials(scen4, po_mat, skeleton_mat, target_pt = 0.25, N = 500)
+scen5 <- c(
+  0.05, 0.15, 0.30,
+  0.07, 0.17, 0.32,
+  0.09, 0.19, 0.35
+)
 
-table(oc1$MTD) / length(oc1$MTD)
-mean(oc1$n_enrolled)
-mean(oc1$adverse_event_rate)
+scen6 <- c(
+  0.05, 0.07, 0.09,
+  0.12, 0.15, 0.18,
+  0.22, 0.25, 0.30
+)
 
-table(oc2$MTD) / length(oc2$MTD)
-table(oc3$MTD) / length(oc3$MTD)
-table(oc4$MTD) / length(oc4$MTD)
+scen7 <- c(
+  0.05, 0.08, 0.10,
+  0.08, 0.15, 0.22,
+  0.10, 0.22, 0.45
+)
+
+scen8 <- c(
+  0.15, 0.20, 0.25,
+  0.20, 0.25, 0.30,
+  0.30, 0.30, 0.35
+)
+
+scen9 <- c(
+  0.30, 0.35, 0.40,
+  0.35, 0.40, 0.50,
+  0.40, 0.45, 0.55
+)
+
+scenario_list <- list(
+  scen1 = scen1,
+  scen2 = scen2,
+  scen3 = scen3,
+  scen4 = scen4,
+  scen5 = scen5,
+  scen6 = scen6,
+  scen7 = scen7,
+  scen8 = scen8,
+  scen9 = scen9
+)
+
+results_list <- list()
+
+for (k in seq_along(scenario_list)) {
+  cat("\n================ Scenario", k, "================\n")
+  scen_vec <- scenario_list[[k]]
+
+  true_tox_mat <- matrix(scen_vec, byrow = TRUE, nrow = 3, ncol = 3)
+  true_tox_vec <- as.vector(true_tox_mat)
+  names(true_tox_vec) <- dose_names
+
+  print(true_tox_vec)
+
+  oc <- simulate_trials(
+    true_tox_vec = true_tox_vec,
+    po_mat = po_mat,
+    skeleton_mat = skeleton_mat,
+    target_pt = target_pt,
+    max_patients = max_patients,
+    N = 1000,
+    verbose = FALSE
+  )
+
+  res_tbl <- clean_results(oc, true_tox_vec)
+  print(res_tbl)
+
+  results_list[[k]] <- res_tbl
+}
 
